6a77818f24c11065a2d77533d6045fe8b587ecf5
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-02-27 21:15:56 +0800

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  age: Number,
+  dateOfBirth: String,
+  password: { type: String, required: true },
+  stshToken: { type: Number, default: 0 },
+  role: { type: String, default: "user" },
+  
+  // 🔥 Transaction History (Reference to Transactions)
+  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }]
+});
+
+const User = mongoose.model("User", userSchema, "Users");
+
+module.exports = User;
