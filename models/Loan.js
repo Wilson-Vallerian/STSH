1dbcc334837b694e9e6d94a9937108a5b188de2c
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-07 07:55:23 +0800

    Update Loan.js

--- models/Loan.js
+++ models/Loan.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const LoanSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  email: { type: String, required: true },
   amount: { type: Number, required: true },
   status: { type: String, enum: ["debt", "paid"], default: "debt" },
-  approval: { type: Boolean, default: null }, // null = pending
+  approval: { type: Boolean, default: false }, // false = not yet approved, true = approved
   createdAt: { type: Date, default: Date.now },
 });
 
