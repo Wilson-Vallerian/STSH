ab71d39b636f977cee84e1688c4b143ca753c4ca
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-18 15:43:24 +0800

    Update Request.js

--- models/Request.js
+++ models/Request.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const requestSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  seed: { type: Number, required: true },
-  dirt: { type: Number, required: true },
+  seedType: { type: String, required: true },
+  seedAmount: { type: Number, required: true },
+  dirtType: { type: String, required: true },
+  dirtAmount: { type: Number, required: true },
+  address: { type: String, required: true },
   status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
-  timestamp: { type: Date, default: Date.now },
+  timestamp: { type: Date, default: Date.now }
 });
 
-const Request = mongoose.model("Request", requestSchema);
+const Request = mongoose.model("Request", requestSchema, "Requests");
+
 module.exports = Request;
