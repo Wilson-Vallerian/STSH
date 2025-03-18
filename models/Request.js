b07495c3acf22d3dd959982400b97c78bf8db4c4
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-18 16:27:10 +0800

    Update Request.js

--- models/Request.js
+++ models/Request.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
-const requestSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  seedType: { type: String, required: true },
-  seedAmount: { type: Number, required: true },
-  dirtType: { type: String, required: true },
-  dirtAmount: { type: Number, required: true },
-  address: { type: String, required: true },
-  status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
-  timestamp: { type: Date, default: Date.now }
-});
+const RequestSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    seedType: { type: String, required: true },
+    seedAmount: { type: Number, required: true },
+    dirtType: { type: String, required: true },
+    dirtAmount: { type: Number, required: true },
+    address: { type: String, required: true },
+    status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
+  },
+  { timestamps: true } // Automatically adds createdAt & updatedAt fields
+);
 
-const Request = mongoose.model("Request", requestSchema, "Requests");
-
-module.exports = Request;
+module.exports = mongoose.model("Request", RequestSchema);
