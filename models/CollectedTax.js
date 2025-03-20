303f2bd21da8b5150b82ca3666ff2ff25dd96f17
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-20 16:31:05 +0800

    Create CollectedTax.js

---
+++ models/CollectedTax.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const collectedTaxSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  email: { type: String, required: true },
+  method: { type: String, enum: ["loan", "transfer"], required: true },
+  taxCollected: { type: Number, required: true },
+  transactionAmount: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const CollectedTax = mongoose.model("CollectedTax", collectedTaxSchema, "CollectedTaxes");
+
+module.exports = CollectedTax;
