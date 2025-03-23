ce231aad3f6e51b0dc688c0cf1e2978cfd2dd9c5
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-23 22:33:17 +0800

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const notificationSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    title: String,
-    message: String,
+    title: { type: String, required: true },
+    message: { type: String, required: true },
     seen: { type: Boolean, default: false },
   },
   { timestamps: true }
