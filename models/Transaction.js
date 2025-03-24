b184d6777a75682fb67bf33c9b5855f1ecc28c3f
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-24 09:38:17 +0800

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
   senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  recipientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  amount: { type: Number, required: true },
+  recipientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+  amount: { type: Number, required: true }, // Original amount
+  tax: { type: Number, default: 0 },
+  totalAmount: { type: Number, default: 0 },
+  method: { type: String, enum: ["transfer", "subscription", "request"], required: true },
   timestamp: { type: Date, default: Date.now },
 });
 
