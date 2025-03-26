2622643eece480fff706c2f44ba32437307278e1
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-26 16:34:00 +0800

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  recipientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  type: {
+    type: String,
+    enum: ["transfer", "insurance", "requestAgriculture"],
+    required: true,
+  },
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Optional if not transfer
+  recipientId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Optional if not transfer
   amount: { type: Number, required: true },
+  tax: { type: Number, default: 0 },
+  total: { type: Number }, // amount + tax
+  insuranceType: { type: String }, // For insurance only
+  planType: { type: String }, // For insurance only
+  seedType: { type: String }, // For agriculture
+  dirtType: { type: String }, // For agriculture
   timestamp: { type: Date, default: Date.now },
 });
 
