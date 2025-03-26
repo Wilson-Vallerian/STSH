bbea63f222addb1f3a43efb26cad4f187b133824
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-26 17:22:10 +0800

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,23 +1,12 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    enum: ["transfer", "insurance", "requestAgriculture"],
-    required: true,
-    default: "transfer",
-  },
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  recipientId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // for non-transfer
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  recipientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   amount: { type: Number, required: true },
-  tax: { type: Number, default: 0 },
-  total: { type: Number, default: 0 },
-  seedType: String,
-  dirtType: String,
-  insuranceType: String,
-  planType: String,
   timestamp: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Transaction", transactionSchema);
+const Transaction = mongoose.model("Transaction", transactionSchema, "Transactions");
+
+module.exports = Transaction;
