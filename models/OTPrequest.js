1e60d8f078eaf647c22c3db3ac1378bd2a1aad4a
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2025-03-30 19:02:04 +0800

    Update OTPrequest.js

--- models/OTPrequest.js
+++ models/OTPrequest.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const otpRequestSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  email: { type: String, required: true }, // ✅ Add this line
   otp: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 180 },
+  createdAt: { type: Date, default: Date.now, expires: 180 }, // expires after 3 minutes
 });
 
 module.exports = mongoose.model("OTPrequest", otpRequestSchema);
